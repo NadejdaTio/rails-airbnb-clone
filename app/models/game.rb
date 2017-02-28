aed5f042abad7885ce1075f91f1523e982f92663
Aurelie Merlo <aurelie.merlo.web@gmail.com>
2017-02-28 10:57:56 +0100

    seed database

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
   belongs_to :profile
+  has_many :availabilities, dependent: :destroy
+  has_many :orders, dependent: :destroy
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :games, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  # has_many :order_reviews, dependent: :destroy
+  has_many :player_reviews, dependent: :destroy
 end
