c8c2f2a6a2b3347d33dc5fe86e73fbba2f50e7c0
Teikiviimoana RIBET <kingofocean@hotmail.com>
2017-03-02 19:18:37 +0100

    Profile Tabs

---
+++ app/controllers/player_reviews_controller.rb
@@ -0,0 +1,23 @@
+class PlayerReviewsController < ApplicationController
+  def index
+    @player_reviews = PlayerReview.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
 
+  get 'player_reviews/index'
+
+  get 'player_reviews/new'
+
+  get 'player_reviews/show'
+
+  get 'player_reviews/create'
+
+  get 'player_reviews/edit'
+
+  get 'player_reviews/update'
+
+  get 'player_reviews/destroy'
+
+  get 'owner_reviews/index'
+
+  get 'owner_reviews/new'
+
+  get 'owner_reviews/show'
+
+  get 'owner_reviews/create'
+
+  get 'owner_reviews/edit'
+
+  get 'owner_reviews/update'
+
+  get 'owner_reviews/destroy'
+
   mount Attachinary::Engine => "/attachinary"
 
   get 'pages/home'
