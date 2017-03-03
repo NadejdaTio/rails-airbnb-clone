cb87fe515933a57249510acdc518edfeca473937
nadejdatio <nadejda.tio@edhec.com>
2017-03-03 15:39:19 +0100

    modifs

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -25,27 +25,33 @@ class OrdersController < ApplicationController
       @order.total_price = days * price
       @order.status = ""
 
-      @profile = Profile.all.select { |profile| profile.user_id == @current_user.id }.first
-      @order.profile = @profile
-      #Vérifications des dates des autres orders
-      arr_dates_of_location = []
+      @profile = Profile.all.select { |profile| profile.user_id == current_user.id }.first
 
-      Order.all.each do |order|
-        if order.game_id == @game.id
-          (order.start_date..order.end_date).each do |date|
-            arr_dates_of_location << date
+      if @profile.nil?
+        redirect_to new_profile_path
+        flash[:alert] = "Veuillez-vous créer un profil"
+      else
+        @order.profile = @profile
+        #Vérifications des dates des autres orders
+        arr_dates_of_location = []
+
+        Order.all.each do |order|
+          if order.game_id == @game.id
+            (order.start_date..order.end_date).each do |date|
+              arr_dates_of_location << date
+            end
           end
         end
-      end
-        #Si les dates sont déjà prises
-        if arr_dates_of_location.include?(@order.start_date) == true || arr_dates_of_location.include?(@order.end_date) == true
-          redirect_to game_path(@game)
-          flash[:alert] = "Les dates choisies ne sont plus disponibles"
-        #Si les dates ne sont pas prises
-        else
-          @order.status = "Demande envoyée"
-          @order.save!
-          redirect_to profile_path(@profile)
+          #Si les dates sont déjà prises
+          if arr_dates_of_location.include?(@order.start_date) == true || arr_dates_of_location.include?(@order.end_date) == true
+            redirect_to game_path(@game)
+            flash[:alert] = "Les dates choisies ne sont plus disponibles"
+          #Si les dates ne sont pas prises
+          else
+            @order.status = "Demande envoyée"
+            @order.save!
+            redirect_to profile_path(@profile)
+          end
         end
     #Si l'utilisateur n'est pas connecté
     else
